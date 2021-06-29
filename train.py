9e10232e54ca542df11abca646b062c8ba457172
ningchao <ningchaoar@163.com>
2021-06-29 14:11:26 +0800

    first runnable demo

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
+import os
 import torch
 import torch.nn as nn
+import numpy as np
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 
@@ -13,12 +15,12 @@ def train():
     couplet_dataset = CustomDataset(config)
     dataloader = DataLoader(couplet_dataset, batch_size=config.batch_size, shuffle=True, collate_fn=couplet_dataset.custom_collate_fn)
     model = PoemModel(config)
-    model.train()  # set mode to train
     loss_fn = nn.CrossEntropyLoss(reduction='mean')
     optimizer = torch.optim.Adam(model.parameters())
     logger_string = "epoch: {}, loss: {}\n"
     print("start training...")
     for epoch in range(config.epoch):
+        model.train()  # set mode to train
         for data in tqdm(dataloader):
             inputs, targets = data
             logits = model(inputs)
@@ -27,6 +29,36 @@ def train():
             optimizer.step()
             optimizer.zero_grad()
         print(logger_string.format(epoch, loss.item()))
+        preview_result(model, config)
+        save_model(model, "model_{}_{:.3f}.pt".format(epoch, loss.item()), config.output_dir)
+
+
+def save_model(model: nn.Module, model_name: str, output_dir: str):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    output_path = os.path.join(output_dir, model_name)
+    torch.save(model.state_dict(), output_path)
+
+
+def preview_result(model: nn.Module, config: Config):
+    model.eval()
+    examples = ["兴漫香江红袖舞",
+                "河伯捧觞，游观沧海",
+                "清风凝白雪",
+                "木",
+                "讲道德，树新风，遵守法纪"]
+    targets = ["龙腾粤海碧云飞",
+               "天帝悬车，凭乘风云",
+               "妙舞散红霞",
+               "花",
+               "学雷锋，做好事，为国利民"]
+    for e, t in zip(examples, targets):
+        ids = torch.Tensor(np.array([config.char2id[c] for c in e])).long().unsqueeze(0)
+        mask = torch.Tensor(np.array([1] * len(e))).unsqueeze(0)
+        logits = model((ids, mask))
+        outputs = torch.argmax(logits, dim=2).long()
+        predict = "".join([config.id2char[i] for i in outputs[0].tolist()])
+        print("input: {}\ntarget: {}\npredict: {}\n".format(e, t, predict))
 
 
 if __name__ == "__main__":
